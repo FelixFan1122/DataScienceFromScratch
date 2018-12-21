fec1889b23a5d71e08d0c74e83c8a8f7673d8abe
FelixFan <zeus_loveship@126.com>
2018-12-21 16:40:54 +0800

    Add matploy

---
+++ VisualizingData/matplot.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot
+
+years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
+gdp = [300.2, 543.3, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
+
+pyplot.plot(years, gdp, color = 'green', marker = 'o', linestyle = 'solid')
+pyplot.title("Nominal GDP")
+pyplot.ylabel("Billions of $")
+pyplot.show()
