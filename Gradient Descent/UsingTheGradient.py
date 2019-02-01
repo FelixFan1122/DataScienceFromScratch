78f508d5faf99f90768290e32a89f311b9d5c85e
FelixFan <zeus_loveship@126.com>
2019-02-01 18:12:07 +0800

    Implement stochastic gradient descent

--- Gradient Descent/UsingTheGradient.py
+++ Gradient Descent/UsingTheGradient.py
@@ -57,6 +57,42 @@ def negate_all(f):
 def maximize_batch(target_fn, gradient_fn, theta_0, tolerance=0.000001):
     return minimize_batch(negate(target_fn), negate_all(gradient_fn), theta_0, tolerance)
 
+def in_random_order(data):
+    indexes = [i for i, _ in enumerate(data)]
+    random.shuffle(indexes)
+    for i in indexes:
+        yield data[i]
+
+def scalar_multiply(c, v):
+    return [c * v_i for v_i in v]
+
+def minimize_stochastic(target_fn, gradient_fn, x, y, theta_0, alpha_0=0.01):
+    data = zip(x, y)
+    theta = theta_0
+    alpha = alpha_0
+    min_theta, min_value = None, float('inf')
+    iterations_with_no_improvement = 0
+
+    while iterations_with_no_improvement < 100:
+        value = sum(target_fn(x_i, y_i, theta for x_i, y_i in data)
+
+        if value < min_value:
+            min_theta, min_value = theta, value
+            iterations_with_no_improvement = 0
+            alpha = alpha_0
+        else:
+            iterations_with_no_improvement += 1
+            alpha *= 0.9
+
+        for x_i, y_i in in_random_order(data):
+            gradient_i = gradient_fn(x_i, y_i, theta)
+            theta = vector_subtract(theta, scalar_multiply(alpha, gradient_i))
+
+    return min_theta
+
+def maximize_stochastic(target_fn, gradient_fn, x, y, theta_0, alpha_0=0.01):
+    return minimize_stochastic(negate(target_fn), negate_all(gradient_fn), x, y, theta_0, alpha_0)
+
 v = [random.randint(-10, 10) for i in range(3)]
 tolerance = 0.0000001
 while True:
